3daeb5cc7707a7cacb754caaa174cf8127f02304
Dima <damiandream@gmail.com>
2022-05-17 14:02:05 +0300

    new if else and switch leason

--- js/script.js
+++ js/script.js
@@ -1,44 +1,41 @@
 "use strict";
 
-/* Задание на урок:
+// if (4 == 9) {
+//     console.log("OK!");
+// } else {
+//     console.log("Error");
+// }
 
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
+// const num = 50;
 
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
+// if (num < 49) {
+//     console.log("Error");
+// } else if (num > 100) {
+//     console.log("To much");
+// } else {
+//     console.log("OK!");
+// }
 
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате: 
-    movies: {
-        'logan': '8.1'
-    }
+// Тернарный оператор
+// const num = 50;
+// (num === 50) ? console.log("OK!") : console.log("Error");
 
-Проверить, чтобы все работало без ошибок в консоли */
+// Бинарный оператор 4+4
+//Унарный оператор +"4"; (унарный плюс который работает с одной строкой)
 
-const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+// const num = 50;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    private: false
-};
-
-const a = prompt("Один из последних просмотренных фильмов?", ""),
-      b = prompt("На сколько оцените его?", ""),
-      c = prompt("Один из последних просмотренных фильмов?", ""),
-      d = prompt("На сколько оцените его?", "");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+// switch (num) {
+//     case 49:
+//         console.log("Error");
+//         break;
+//     case 100:
+//         console.log("Error");
+//         break;
+//     case 50:
+//         console.log("OK");
+//         break;
+//     default:
+//         console.log("Not this time");
+//         break;
+// }
