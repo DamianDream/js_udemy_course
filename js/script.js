e0aef0d4cfc467c8dd681b39f43f66325545dac3
Dima <damiandream@gmail.com>
2022-05-17 12:54:24 +0300

    first practice done

--- js/script.js
+++ js/script.js
@@ -23,3 +23,22 @@
 
 Проверить, чтобы все работало без ошибок в консоли */
 
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt("Один из последних просмотренных фильмов?", ""),
+      b = prompt("На сколько оцените его?", ""),
+      c = prompt("Один из последних просмотренных фильмов?", ""),
+      d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
