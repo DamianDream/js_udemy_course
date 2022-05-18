376e346ec04dc57ce3e3c88513e24bab759efd75
Dima <damiandream@gmail.com>
2022-05-18 09:57:47 +0300

    Script FOR

--- js/script.js
+++ js/script.js
@@ -1,2 +1,54 @@
 "use strict";
 
+// let num = 50;
+//первый цыкл
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
+// }
+
+//второй цыкл
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+//третий цыкл
+// for (let i = 1; i < 8; i++) {
+//     console.log(i);
+// }
+
+//цыкл выполняется до определенного утановленного значения
+//цыкл продолжается исключая определенный заданный шаг
+// for (let i = 1; i < 8; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+//         for (let j = 0; j < 3; j++) {
+//         console.log(j);
+//     }
+// }
+
+//задачка 
+// *
+// **
+// ***
+// ****
+// *****
+
+let result = "";
+const lengthNumber = 7;
+
+for (let i = 1; i < lengthNumber; i++) {
+    
+}
+
+
+console.log(result);
