1aec06ce4f7e82862bbf420d3c300f8b77058fdd
Dima <damiandream@gmail.com>
2022-05-19 23:25:04 +0300

    Foo practice

--- js/foo.js
+++ js/foo.js
@@ -79,3 +79,70 @@
 // function doNothing() { } //true
 // console.log(doNothing() === undefined);
 
+'use strict';
+
+// Место для первой задачи //Привет, Антон
+// function sayHello(user) {
+//     return "Привет, " + user + "!";
+// }
+// const user = "Антон";
+// console.log(sayHello(user));
+
+// Место для первой задачи //Привет, Антон (ответ автора ++
+// function sayHello(name) {
+//     return `Привет, ${name}!`;
+// }
+
+// console.log(sayHello("Alex"));
+
+// Место для второй задачи
+// function returnNeighboringNumbers(simpleNumber) {
+//     const tempArr = [];
+//     for (let i = 0; i < 3; i++) {
+//         tempArr[i] = simpleNumber - 1;
+//           simpleNumber++;
+//     }
+//     return tempArr;
+// }
+
+// const simpleNumber = 5;
+// console.log(returnNeighboringNumbers(simpleNumber));
+
+// Место для второй задачи //ответ атора +++++
+// function returnNeighboringNumbers(num) {
+//     return [num - 1, num, num + 1];
+// }
+// console.log(returnNeighboringNumbers(5));
+
+
+// Место для третьей задачи
+// function getMathResult(sum, count) {
+//     if (typeof(count) !== 'number' || count <= 0) {
+//         return sum;
+//     }
+
+//     let str = '';
+
+//     for (let i = 1; i <= count; i++) {
+//         if (i === count) {
+//             str += `${sum * i}`; // Тут без черточек в конце
+//         } else {
+//             str += `${sum * i}---`; // Это тоже самое, что и str = str + num * i + "---"
+//         }
+//     }
+//     return str;
+// }
+// console.log(getMathResult(10, 5));
+
+// const lines = 5;
+// let result = '';
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+// console.log(result);
